69026eed6dac5f3415d7ca6066235133be35df36
daveontour <dave_on_tour@yahoo.com>
2018-10-23 21:27:28 +0400

    BE2

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-  baseURL: string = 'http://aidx.quaysystems.com.au';
-//  baseURL: string = '.';
+ // baseURL: string = 'http://aidx.quaysystems.com.au';
+  baseURL: string = 'http://localhost:8080/XSD_Forms/json';
   xmlMessage: string = "";
   sampleXMLMessage: string = "";
   root:any;
